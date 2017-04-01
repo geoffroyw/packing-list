a59198509d6e49e14a29658bd2bd38046d2d964a
Geoffroy Weyer <webweyer@gmail.com>
2017-04-01 17:01:06 +0200

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :packages do
     resources :items, controller: 'package_items'
   end
+
+  root to: 'packages#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
