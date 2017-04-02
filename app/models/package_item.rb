63e9418eaeb68eb2b2840e69a2cd66f1fad82f1e
Geoffroy Weyer <webweyer@gmail.com>
2017-04-02 16:19:42 +0200

    Only show unpacked items

--- app/models/package_item.rb
+++ app/models/package_item.rb
@@ -7,4 +7,14 @@ class PackageItem < ApplicationRecord
 
   validates_numericality_of :quantity, greater_than_or_equal_to: 1, only_integer: true, allow_nil: false, allow_blank: false
 
+  validate :not_too_high_quantity
+
+  private
+
+  def not_too_high_quantity
+    if item.present? && item.quantity > quantity
+      errors.add(:quantity, "exceeds the total quantity of the item")
+    end
+  end
+
 end
